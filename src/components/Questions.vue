fdaa4574e877c2c6543a5e26def0affde3e9a1d9
Md Dilshad Alam <mddalam1@gmail.com>
2021-11-06 22:31:01 +0530

    Quiz Project Completed.

---
+++ src/components/Questions.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div
+        class="bar"
+        :style="{ width: `${(questionAnswered / questions.length) * 100}%` }"
+      ></div>
+      <div class="status">
+        {{ questionAnswered }} out of {{ questions.length }} questions answered
+      </div>
+    </div>
+    <div class="single-question">
+      <div class="question">{{ questions[questionAnswered].q }}</div>
+      <div class="answers">
+        <div
+          class="answer"
+          v-for="answer in questions[questionAnswered].answers"
+          :key="answer.text"
+          @click="this.$emit('answered', answer.is_correct)"
+        >
+          {{ answer.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Questions",
+  props: ["questionAnswered", "questions"],
+};
+</script>
+
+<style></style>
---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="result">
+    <div class="title">{{ title }}</div>
+    <div class="desc">{{ description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Result",
+  props: ["results", "correctAnswers"],
+  computed: {
+    title() {
+      const result = this.results.find(
+        (result) =>
+          result.min <= this.correctAnswers && result.max >= this.correctAnswers
+      );
+      return result.title;
+    },
+    description() {
+      const result = this.results.find(
+        (result) =>
+          result.min <= this.correctAnswers && result.max >= this.correctAnswers
+      );
+      return result.desc;
+    },
+  },
+};
+</script>
+
+<style></style>
